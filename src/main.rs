e91f512b895f99aa1c1faceff4b3aceec272fdf6
Vinícius Simões <vinicius.simoes95@gmail.com>
2018-09-23 18:47:00 -0300

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+extern crate rand;
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("Adivinhe o número:");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    loop {
+        println!("Digite seu palpite: ");
+
+        let mut palpite = String::new();
+
+        io::stdin().read_line(&mut palpite).expect(
+            "Falha ao ler o palpite",
+        );
+
+        let palpite: u32 = match palpite.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("Você disse: {}", palpite);
+
+        match palpite.cmp(&secret_number) {
+            Ordering::Less => println!("Muito baixo!"),
+            Ordering::Greater => print!("Muito alto!"),
+            Ordering::Equal => {
+                println!("Você acertou!");
+                break;
+            }
+        }
+    }
+}
