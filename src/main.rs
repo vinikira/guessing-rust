1cf16e1cb15f82b3382133e2ea188dfde29ea37e
Vinícius Simões <vinicius.simoes95@gmail.com>
2018-10-19 19:26:22 -0300

    add guess type

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,23 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
+pub struct Guess {
+    value: u32,
+}
+
+impl Guess {
+    pub fn new(value: u32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}", value)
+        }
+
+        Guess { value }
+    }
+    pub fn value(&self) -> u32 {
+        self.value
+    }
+}
+
 fn main() {
     println!("Adivinhe o número:");
 
@@ -18,14 +35,14 @@ fn main() {
             .read_line(&mut palpite)
             .expect("Falha ao ler o palpite");
 
-        let palpite: u32 = match palpite.trim().parse() {
-            Ok(num) => num,
+        let palpite: Guess = match palpite.trim().parse() {
+            Ok(num) => Guess::new(num),
             Err(_) => continue,
         };
 
-        println!("Você disse: {}", palpite);
+        println!("Você disse: {}", palpite.value());
 
-        match palpite.cmp(&secret_number) {
+        match palpite.value().cmp(&secret_number) {
             Ordering::Less => println!("Muito baixo!"),
             Ordering::Greater => print!("Muito alto!"),
             Ordering::Equal => {
